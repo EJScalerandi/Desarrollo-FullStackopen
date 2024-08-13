5127f0cc751f9b82e5730a3f16fbc3840aee05ab
EJScalerandi <estebanscalerandi.dev@gmail.com>
2024-08-13 18:53:46 -0300

    vamos por el 1.09

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,19 +1,46 @@
 import { useState } from 'react'
 
+const Statistics = (props)=>{
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <h3>good {props.good}</h3>
+      <h3>neutral {props.neutral}</h3>
+      <h3>bad {props.bad}</h3>
+      <h3>all {props.all}</h3>
+      <h3>average {props.average}</h3>
+      <h3>positive {props.positive}</h3>
+    </div>
+  )
+
+  
+}
+
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [average, setAverage] = useState(0)
+
 
   const handleGoodClick = ()=>{
     setGood(good + 1)
+    setAll(all + 1)
+    setPositive(good/all)
+    setAverage((good - bad) / all)
   };
   const handleNeutralClick = ()=>{
     setNeutral(neutral + 1)
+    setAll(all + 1)
   };
   const handleBadClick = ()=>{
     setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((good - bad) / all)
   }
 
   return (
@@ -22,10 +49,11 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <h1>Statistics</h1>
-      <h3>good {good}</h3>
-      <h3>neutral {neutral}</h3>
-      <h3>bad {bad}</h3>
+      
+      <Statistics good={good} neutral={neutral} bad={bad}
+      all={all} positive={positive} average={average}/>
+      
+      
     </div>
       )
 }
